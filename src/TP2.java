9060a22c0ff763aa72092054de1a1af35621b4ed
matheusvargas <matheus.vargas@tatic.net>
2018-10-05 17:56:49 -0300

    Commit incial.

--- src/TP2.java
+++ src/TP2.java
@@ -2,17 +2,33 @@
 class Circle {
     private double radius;
     private String color;
-    public Circle(){radius = 1.0; color = "red";}
-    public Circle(double r){this(); this.radius = r; color = "red";}
-    public double getRadius(){ return radius; }
+    private double length;
+    public Circle(){this.radius = 1.0; this.color = "red"; this.length = calculateLength(this.radius)}
+    public Circle(double r){this(); this.radius = r; this.color = "red";}
+    public Circle(double r, String s){this(); this.radius = r; this.color = s;}
+    public double getRadius(){ return this.radius; }
+    public double getColor(){ return this.color; }
     public double getArea(){ return radius*radius*Math.PI;}
+    public void setRadius(double radius) {this.radius = radius;}
+    public void setColor(String color) {this.color = color};
+    private double calculateLength(double radius){return 2*radius*Math.PI;}
+    public String toString(){ return "Circulo: raio = " + radius + " cor = " + color};
+}
+
+public class Point {
+    private float x, y;
+    public float getX(){return this.x};
+    public float getY(){return this.y}
+    public setCoordinates(float x, float y){this.x = x; this.y = y;}
+    public String toString(){ return "Centro nas coordenadas = " + this.x + "," + this.y};
 }
 
 public class TP2 {
     public static void main(String [] args) {
-        Circle c1, c2;
+        Circle c1, c2, c3;
         c1 = new Circle();
         c2 = new Circle(2.0);
+        c3 = new Circle(3.0, purple);
         System.out.println("O circulo tem o raio de " + c1.getRadius()
                 + " e area de " + c1.getArea());
         System.out.println("O circulo tem o raio de " + c2.getRadius()
